ea3448ae119cebe3c984c06bde10b6171a85c671
Gozi <ziv.yuan@gmail.com>
2023-08-07 04:57:04 +0800

    zui-svg-icon

---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="content">
+    <view class="container">
+      <view v-for="icon in iconLib" :key="icon.key" class="icon-item">
+        <view class="icon">
+          <zui-svg-icon :icon="icon.key" />
+        </view>
+        <view class="title">{{ icon.key }}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import zuiSvgIcon from "../../uni_modules/zui-svg-icon/components/zui-svg-icon/zui-svg-icon.vue";
+import IconLib from "@/static/svg-icons-lib.js";
+
+export default {
+  components: {
+    zuiSvgIcon,
+  },
+
+  data() {
+    const lib = Object.keys(IconLib)
+      .map((key) => ({ key, icon: IconLib[key] }))
+    return {
+      iconLib: lib,
+    };
+  },
+
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 48rpx;
+}
+.icon-item {
+  width: 25%;
+  text-align: center;
+  margin-bottom: 2em;
+}
+
+.icon {
+  font-size: 80rpx;
+}
+</style>
---
+++ uni_modules/zui-svg-icon/components/zui-svg-icon/zui-svg-icon.vue
@@ -0,0 +1,148 @@
+<template>
+  <view class="zui-svg-icon" :style="style">
+    <image
+      class="zui-svg-icon-image"
+      src="../../static/zui-svg-icon/zui-svg-icon-placeholder.svg"
+      mode="aspectFill"
+    ></image>
+  </view>
+</template>
+
+<script>
+import IconLib from "@/static/svg-icons-lib.js";
+
+const regColorProps = /(?:fill|stroke)="([^"]+)"/g;
+
+export default {
+  name: "zui-svg-icon",
+
+  components: {},
+
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+
+    /**
+     * 图标颜色
+     *
+     * 单色图标: '#FFF'
+     *
+     * 多色图标: ['#FFF', '#FFF', '#FFF'], 颜色数量需要匹配
+     */
+    color: [String, Array],
+
+    width: {
+      type: [Number, String],
+      default: "1.2em",
+    },
+
+    aspectRatio: {
+      type: Number,
+      default: 1,
+    },
+  },
+
+  data() {
+    return {
+      isFilled: false,
+    };
+  },
+
+  computed: {
+    // 返回色彩列表
+    multiColors() {
+      const icon = IconLib[this.icon];
+      let colors = [...icon.matchAll(regColorProps)]
+        .filter((item) => item[1] !== "none")
+        .map((item) => item[1]);
+      colors = Array.from(new Set(colors));
+
+      return colors;
+    },
+
+    style() {
+      let icon = IconLib[this.icon];
+      if (!icon)
+        throw new Error(
+          `Svg icon [${this.icon}] not defined and no fallback icon set.`
+        );
+
+      const width =
+        typeof this.width === "number" ? `${this.width}px` : this.width;
+      const style = {
+        "--zui-svg-icon-width": width,
+        "--zui-svg-icon-aspect-ratio": this.aspectRatio,
+      };
+
+      let svg = icon;
+      if (this.multiColors.length > 1 && this.color) {
+        const color =
+          typeof this.color === "string" ? this.color.split(",") : this.color;
+        if (color.length >= this.multiColors.length) {
+          // 填充多色
+          svg = icon.replace(regColorProps, (_, color) => {
+            if (color === "none") return _;
+            const idx = this.multiColors.findIndex((item) => item === color);
+            return `fill="${color[idx]}"`;
+          });
+        } else {
+          if (color.length < this.multiColors.length) {
+            console.warn(
+              `多色图标 ${this.icon} 需要 ${this.multiColors.length} 个颜色, 但只提供了 ${color.length}。`
+            );
+          }
+        }
+      } else if (/^#[0-9A-F]+$/i.test(this.color)) {
+        svg =
+          icon.indexOf("fill=") === -1
+            ? icon.replace(/<(path|p)( |>)/g, `<$1 fill="${this.color}"$2`)
+            : icon.replace(
+                regColorProps,
+                (_, v) => `fill="${v === "none" ? v : this.color}"`
+              );
+      }
+
+      style[
+        "--zui-svg-icon-image"
+      ] = `url('data:image/svg+xml,${encodeURIComponent(svg)}')`;
+      return style;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.zui-svg-icon {
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: var(--zui-svg-icon-width);
+  height: auto;
+  aspect-ratio: var(--zui-svg-icon-aspect-ratio);
+  line-height: 1;
+  vertical-align: middle;
+  margin: 0 0.2em;
+
+  &:first-child {
+    margin-left: 0;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+
+  .zui-svg-icon-image {
+    width: 100%;
+    height: 100%;
+
+    ::v-deep {
+      > div {
+        background-image: var(--zui-svg-icon-image) !important;
+        background-size: contain !important;
+      }
+    }
+  }
+}
+</style>
