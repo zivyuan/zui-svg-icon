0d0f155b0dcb3575cd7377219bf6f7deca5c9a9b
zivyuan <ziv.yuan@gmail.com>
2023-10-08 21:41:19 +0800

    更新发布脚本

--- scripts/pubh5.sh
+++ scripts/pubh5.sh
@@ -14,7 +14,7 @@ cd $BASE/../dist/build/
 rm -f dist.tar.gz
 tar --no-xattrs --exclude=._* --exclude=.DS_Store -czvf dist.tar.gz h5
 
-ssh ubuntu@lighthouse.ip.imgozi.cn "rm -rf ${path}; mkdir ${path};"
-scp dist.tar.gz "ubuntu@lighthouse.ip.imgozi.cn:${path}"
-ssh ubuntu@lighthouse.ip.imgozi.cn "cd ${path}; tar --exclude=._* --exclude=.DS_Store --strip-components 1 -xzvf dist.tar.gz"
+ssh ubuntu@virtual.uni-demo.com "rm -rf ${path}; mkdir ${path};"
+scp dist.tar.gz "ubuntu@virtual.uni-demo.com:${path}"
+ssh ubuntu@virtual.uni-demo.com "cd ${path}; tar --exclude=._* --exclude=.DS_Store --strip-components 1 -xzvf dist.tar.gz"
 
